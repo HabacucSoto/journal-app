8475cdaab6517bd8be2b4ff27323072981fddcac
habacuc <josehabacuc182020@gmail.com>
2022-05-07 10:48:38 -0500

    added mutations in journal vuex module

--- src/modules/daybook/store/journal/mutations.js
+++ src/modules/daybook/store/journal/mutations.js
@@ -1,3 +1,17 @@
 // ? Como tiene que verse el mutation
 // export const myMutation = ( state ) => {
 // }
+
+// ! Si no se usa el state se puede comentar para no tener problemas con linter
+
+export const setEntries = ( /*state*/ ) => {
+
+}
+
+export const updateEntry = ( /*state*/ ) => {
+    
+}
+
+export const addEntry = ( /*state*/ ) => {
+    
+}
