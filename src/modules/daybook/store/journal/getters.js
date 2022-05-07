56bc25af090a45b7a4ab9a06cc3d095851e8df2d
habacuc <josehabacuc182020@gmail.com>
2022-05-07 10:51:25 -0500

    added getters in journal vuex module

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -2,3 +2,13 @@
 // export const myGetter = ( state ) => {
     // return state
 // }
+
+// ! Si no se usa state se puede comentar para no tener problemas con linter 
+
+export const getEntriesByTerm = ( /*state*/ ) => {
+    
+}
+
+export const getEntryById = ( /*state*/ ) => {
+    
+}
