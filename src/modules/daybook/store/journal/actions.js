5711ae409ed0133f29d7e5f173748816e9fa1044
habacuc <josehabacuc182020@gmail.com>
2022-05-07 10:46:01 -0500

    added actions in journal vuex module

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -1,4 +1,17 @@
 // ? Como tiene que verse el action
 // export const myAction = async ({ commit }) => {
-
 // }
+
+// ! Si no se usa el commit se puede comentar 
+
+export const loadEntries = async (/*{ commit }*/) => {
+
+}
+
+export const updateEntry = async (/*{ commit }*/) => {
+
+}
+
+export const createEntry = async (/*{ commit }*/) => {
+
+}
